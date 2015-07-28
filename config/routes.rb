dc1eb3edd7e4a956f4b9ac2ff8f681a7cbc4dc63
solli86 <paul-sart@yandex.ru>
2015-07-28 14:59:17 +0300

    fix rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'movie/movies'
+  get 'movies/index'
+
+  get 'movies/show'
+
+resources :movies
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
